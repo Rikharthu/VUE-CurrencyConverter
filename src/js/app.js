ebea6cb6b9d3f2db269de6dec2f0f87e42c19275
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-05-02 14:54:52 +0300

    saving currencies in localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,29 @@ new Vue({
   },
 
   mounted() {
-    axios.get('https://free.currconv.com/api/v7/currencies?apiKey=de7c25351319ee026c9b')
-      .then(response => {
-        console.log(response);
-        this.currencies = response.data.results
+    this.getCurrencies();
+  },
+
+  methods: {
+    getCurrencies() {
+
+      // gets currencies from storage of browser
+      const currencies = localStorage.getItem('currencies');
+
+      // if it exists, assigns VUE variable currencies and exits the function
+      if (currencies) {
+
+        this.currencies = JSON.parse(currencies);
+        return;
+      }
+
+      // else makes request to api and saving it
+      axios.get('https://free.currconv.com/api/v7/currencies?apiKey=de7c25351319ee026c9b')
+        .then(response => {
+
+          this.currencies = response.data.results
+          localStorage.setItem('currencies', JSON.stringify(response.data.results))
       })
+    }
   }
 });
